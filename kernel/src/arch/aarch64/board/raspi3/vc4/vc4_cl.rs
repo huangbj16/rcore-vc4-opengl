0fc7f9c69ec3a803ac6903ce4eb4551794158bcd
Bingjian(Alex) Huang <huangbj16@mails.tsinghua.edu.cn>
2019-10-28 11:08:24 +0800

    erase some unsafe in vc4_cl.rs

--- kernel/src/arch/aarch64/board/raspi3/vc4/vc4_cl.rs
+++ kernel/src/arch/aarch64/board/raspi3/vc4/vc4_cl.rs
@@ -5,29 +5,26 @@ struct vc4_bo;
 struct vc4_context;
 
 struct vc4_cl {
-    base: *void;
-    next: *void;
-    void *reloc_next;
-    size_t size;
+    base: usize;
+    next: usize;
+    reloc_next: usize;
+    size: size_t;//what is size_t
 }
 
-pub fn vc4_init_cl(cl: *mut vc4_cl){
-    unsafe {
-        (*cl).base = 0;
-        (*cl).next = 0;
-        (*cl).size = 0;
-    }
+//in C, parameter is a pointer vc4_cl* cl; in Rust, I use reference &mut vc4_cl to be safe.
+pub fn vc4_init_cl(cl: &mut vc4_cl){
+    cl.base = 0;
+    cl.next = 0;
+    cl.size = 0;
 }
-pub fn vc4_reset_cl(cl: *mut vc4_cl){
-    unsafe {
-        (*cl).next = (*cl).base;
-    }
+pub fn vc4_reset_cl(cl: &mut vc4_cl){
+    cl.next = cl.base;
 }
-pub fn vc4_dump_cl(cl: *mut vc4_cl, size: &mut u32, cols: &mut u32, name: &str){
+pub fn vc4_dump_cl(cl: &mut vc4_cl, size: &u32, cols: &u32, name: &str){
     println!("vc4_dump_cl {}:", name);
 
-    let &mut offset = 0;
-    let mut ptr: *mut u8 = cl as *mut u8;
+    let mut offset = 0;
+    let mut ptr: u8 = cl as u8;
 
     while offset < size {
         print!("{08x}: ", ptr);
@@ -47,48 +44,54 @@ pub fn vc4_dump_cl(cl: *mut vc4_cl, size: &mut u32, cols: &mut u32, name: &str){
 //struct __attribute__((__packed__)) unaligned_16 { uint16_t x; };
 //struct __attribute__((__packed__)) unaligned_32 { uint32_t x; };
 
-pub fn cl_offset(cl: *mut vc4_cl) -> u32
+pub fn cl_offset(cl: &mut vc4_cl) -> u32
 {
-    unsafe {
-        ((*cl).next as u32) - ((*cl).base as u32)
+    (cl.next as u32) - (cl.base as u32)
+}
+
+pub fn cl_advance(cl: &mut vc4_cl, n: u32){
+    cl.next += n;
+}
+
+pub fn put_unaligned_32(ptr: &mut u32, val: u32)
+{
+    ptr = val;
+}
+
+pub fn put_unaligned_16(ptr: &mut u16, val: u16)
+{
+    ptr = val;
+}
+
+//not needed any more?
+
+// pub fn get_unaligned_32(ptr: usize) -> u32
+// {
+// struct unaligned_32 *p = (void *)ptr;
+// return p->x;
+// }
+
+// pub fn get_unaligned_16(ptr: usize) -> u16
+// {
+// struct unaligned_16 *p = (void *)ptr;
+// return p->x;
+// }
+
+pub fn cl_u8(cl: &mut vc4_cl, n: u8)
+{
+    unsafe{
+        let *mut next = cl.next as u8;
+        *next = n;
     }
+    cl_advance(cl, 1);
 }
 
-pub fn cl_advance(cl: *mut vc4_cl, n: u32){
-    unsafe {
-        (*cl).next += n;
-    }
-}
-
-pub fn put_unaligned_32(void *ptr, val: u32)
-{
-struct unaligned_32 *p = (void *)ptr;
-p->x = val;
-}
-
-pub fn put_unaligned_16(void *ptr, val: u16)
-{
-struct unaligned_16 *p = (void *)ptr;
-p->x = val;
-}
-
-pub fn get_unaligned_32(void *ptr) -> u32
-{
-struct unaligned_32 *p = (void *)ptr;
-return p->x;
-}
-
-pub fn get_unaligned_16(void *ptr) -> u16
-{
-struct unaligned_16 *p = (void *)ptr;
-return p->x;
-}
-
-pub fn cl_u8(cl: *mut vc4_cl, n: u8)
-{
-*(uint8_t *)cl->next = n;
-cl_advance(cl, 1);
-}
+//not very sure about how to convert uint8_t *?
+// static inline void cl_u8(struct vc4_cl *cl, uint8_t n)
+// {
+// 	*(uint8_t *)cl->next = n;
+// 	cl_advance(cl, 1);
+// }
 
 pub fn cl_u16(cl: *mut vc4_cl, n: u16)
 {
